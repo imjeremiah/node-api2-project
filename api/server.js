43f3aae37723056486764d01784bd0a9c0935cfb
Jeremiah Candelaria <jeremiahcandelaria@gmail.com>
2022-02-01 16:28:25 -0800

    add cors, complete stretch goals

--- api/server.js
+++ api/server.js
@@ -1,11 +1,14 @@
 // implement your server here
 const express = require('express');
+const cors = require('cors');
 
 const postsRouter = require('./posts/posts-router');
 
 const server = express();
 
 server.use(express.json());
+
+server.use(cors());
 
 // require your posts router and connect it here
 server.use('/api/posts', postsRouter);
